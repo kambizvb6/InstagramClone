07e20bd16d690298b93661f7225aa73347d9ee75
kambizvb6 <kambizvb6@gmail.com>
2023-11-17 13:42:50 -0600

    FeedView

---
+++ InstagramTutorial/Core/Feed/FeedCell.swift
@@ -0,0 +1,18 @@
+//
+//  FeedCell.swift
+//  InstagramTutorial
+//
+//  Created by Kambiz on 2023-11-17.
+//
+
+import SwiftUI
+
+struct FeedCell: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FeedCell()
+}
---
+++ InstagramTutorial/Core/Feed/FeedView.swift
@@ -0,0 +1,18 @@
+//
+//  FeedView.swift
+//  InstagramTutorial
+//
+//  Created by Kambiz on 2023-11-17.
+//
+
+import SwiftUI
+
+struct FeedView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FeedView()
+}
