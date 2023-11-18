f912df61f29771bc272fcec10bbc24b7fc74f097
kambizvb6 <kambizvb6@gmail.com>
2023-11-18 00:27:27 -0600

    AddEmailView, IGTextFieldModifier

---
+++ InstagramTutorial/Core/Authentication/AddEmailView.swift
@@ -0,0 +1,18 @@
+//
+//  AddEmailView.swift
+//  InstagramTutorial
+//
+//  Created by Kambiz on 2023-11-18.
+//
+
+import SwiftUI
+
+struct AddEmailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddEmailView()
+}
---
+++ InstagramTutorial/Core/Components/IGTextFieldModifier.swift
@@ -0,0 +1,8 @@
+//
+//  IGTextFieldModifier.swift
+//  InstagramTutorial
+//
+//  Created by Kambiz on 2023-11-18.
+//
+
+import Foundation
