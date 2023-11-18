cbbc7009c0a2a28e20e7b5da8576206f15e66251
kambizvb6 <kambizvb6@gmail.com>
2023-11-17 22:46:00 -0600

    LoginView

---
+++ InstagramTutorial/Core/Authentication/LoginView.swift
@@ -0,0 +1,18 @@
+//
+//  LoginView.swift
+//  InstagramTutorial
+//
+//  Created by Kambiz on 2023-11-17.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoginView()
+}
