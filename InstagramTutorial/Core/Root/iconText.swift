ea070364e49b8393fc72a7d06bc68af32c59916a
kambizvb6 <kambizvb6@gmail.com>
2023-11-19 08:12:28 -0600

    Icon, UploadPostView

---
+++ InstagramTutorial/Core/Root/iconText.swift
@@ -0,0 +1,18 @@
+//
+//  iconText.swift
+//  InstagramTutorial
+//
+//  Created by Kambiz on 2023-11-18.
+//
+
+import SwiftUI
+
+struct iconText: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    iconText()
+}
---
+++ InstagramTutorial/Core/UploadPost/UploadPostView.swift
@@ -0,0 +1,18 @@
+//
+//  UploadPostView.swift
+//  InstagramTutorial
+//
+//  Created by Kambiz on 2023-11-18.
+//
+
+import SwiftUI
+
+struct UploadPostView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UploadPostView()
+}
